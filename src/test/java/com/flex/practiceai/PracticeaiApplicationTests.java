a1b702e5a3d4b85c1b7f5b111aeee0178742ae96
Luan de Oliveira <luan012012@hotmail.com>
2024-04-20 16:42:11 -0300

    Primeiro commit

---
+++ src/test/java/com/flex/practiceai/PracticeaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flex.practiceai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
